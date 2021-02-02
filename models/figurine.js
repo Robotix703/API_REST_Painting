06128957e0cbd1bf282a187141ae4105eff27722
Robotix703 <stephane.chatelle@gmail.com>
2021-02-02 19:47:54 +0100

    Correction problème création figurine

--- models/figurine.js
+++ models/figurine.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const figurineSchema = mongoose.Schema({
     name: { type: String, required: true},
     categorie: { type: String, required: true },
-    imagePath: { type: String, required: true},
-    resultPath: { type: String, required: true}
+    imagePath: { type: String, required: true}
 });
 
 module.exports = mongoose.model('Figurine', figurineSchema);
